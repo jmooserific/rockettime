bd8685f66930b5bf04f5f24c33742a2841d4fd65
John Moose <john@tinyairplane.com>
2017-03-06 22:09:06 -0500

    Added stub of detail modal.

---
+++ src/LaunchDetails.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './LaunchDetails.scss';
+
+class LaunchDetails extends Component {
+
+  render() {
+    return (
+      <div className="LaunchDetails">
+        <p>Details go here.</p>
+      </div>
+    );
+  }
+}
+
+export default LaunchDetails;
--- src/LaunchItem.jsx
+++ src/LaunchItem.jsx
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
+import { Modal, Button } from 'react-bootstrap';
 import RocketImage from './RocketImage.jsx';
+import LaunchDetails from './LaunchDetails.jsx';
 import Time from 'react-time'
 import Moment from 'moment';
+import './LaunchItem.scss';
 
 class LaunchItem extends Component {
 
@@ -9,8 +12,20 @@ class LaunchItem extends Component {
     super(props);
     this.state = {
       time: Moment(this.props.launch.net, 'MMMM D, YYYY HH:mm:ss UTC'),
+      showDetailsModal: false
     };
     this.statusNames = ['Green', 'Red', 'Success', 'Failed'];
+
+    this.openDetailsModal = this.openDetailsModal.bind(this);
+    this.closeDetailsModal = this.closeDetailsModal.bind(this);
+  }
+
+  openDetailsModal() {
+    this.setState({ showDetailsModal: true });
+  }
+
+  closeDetailsModal() {
+    this.setState({ showDetailsModal: false });
   }
 
   classNames() {
@@ -22,12 +37,22 @@ class LaunchItem extends Component {
 
   render() {
     return (
-      <div className={this.classNames()}>
-        <RocketImage name={this.props.launch.name.split(' | ')[0]} key={'rocketImage' + this.props.launch.id} />
+      <a className={this.classNames()} onClick={this.openDetailsModal}>
+        <RocketImage name={this.rocketName} key={'rocketImage_' + this.props.launch.id} />
         <span className="name">{this.props.launch.name}</span>
         <span className="time"><Time value={this.props.launch.net} format="MM/DD/YYYY" valueFormat="MMMM D, YYYY HH:mm:ss UTC" /></span>
+
         <div className="circle" />
-      </div>
+
+        <Modal show={this.state.showDetailsModal} onHide={this.closeDetailsModal}>
+          <Modal.Header closeButton>
+            <Modal.Title>{this.props.rocketName}<br/><small>{this.props.missionName}</small></Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <LaunchDetails key={'rocketDetails_' + this.props.launch.id} />
+          </Modal.Body>
+        </Modal>
+      </a>
     );
   }
 }
