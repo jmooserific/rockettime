c6b36dcb2bd1f9e12791c7976642d5d14e4ab895
John Moose <john@tinyairplane.com>
2017-03-14 21:21:53 -0400

    Use react-bootstrap for navbar

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,23 +1,19 @@
 import React, { Component } from 'react';
+import { Navbar } from 'react-bootstrap';
+import './Nav.scss';
+
 
 class Nav extends Component {
   render() {
     return (
-      <nav className="Nav navbar navbar-fixed-top">
-        <div className="container">
-          <div className="navbar-header">
-            <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-              <span className="sr-only">Toggle navigation</span>
-              <span className="icon-bar"></span>
-              <span className="icon-bar"></span>
-              <span className="icon-bar"></span>
-            </button>
-            <a className="navbar-brand" href="#"><span>rocket</span>time!</a>
-          </div>
-          <div id="navbar" className="collapse navbar-collapse">
-          </div>
-        </div>
-      </nav>
+      <Navbar fixedTop>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <a href="#"><span>rocket</span>time!</a>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+      </Navbar>
     );
   }
 }
